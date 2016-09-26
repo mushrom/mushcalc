af982a2d1153d25438b0edae40b7b0d73b562325
My Name <foo@lisp.us.to>
2016-09-26 14:38:34 -0400

    Adding handlers for button clicks

--- calc.cpp
+++ calc.cpp
@@ -1,5 +1,6 @@
 #include "calc.h"
 #include "ui_calc.h"
+#include <cstdio>
 
 calc::calc(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,35 @@ calc::calc(QWidget *parent) :
 calc::~calc()
 {
     delete ui;
+}
+
+void calc::insert_str( const QString &str ){
+    ui->lineEdit->setText( ui->lineEdit->text() + str );
+}
+
+void calc::on_pushButton_n1_clicked() { insert_str( "1" ); }
+void calc::on_pushButton_n2_clicked() { insert_str( "2" ); }
+void calc::on_pushButton_n3_clicked() { insert_str( "3" ); }
+void calc::on_pushButton_n4_clicked() { insert_str( "4" ); }
+void calc::on_pushButton_n5_clicked() { insert_str( "5" ); }
+void calc::on_pushButton_n6_clicked() { insert_str( "6" ); }
+void calc::on_pushButton_n7_clicked() { insert_str( "7" ); }
+void calc::on_pushButton_n8_clicked() { insert_str( "8" ); }
+void calc::on_pushButton_n9_clicked() { insert_str( "9" ); }
+void calc::on_pushButton_n0_clicked() { insert_str( "0" ); }
+void calc::on_pushButton_open_paren_clicked()  { insert_str( "(" ); }
+void calc::on_pushButton_close_paren_clicked() { insert_str( ")" ); }
+void calc::on_pushButton_plus_clicked()        { insert_str( "+" ); }
+void calc::on_pushButton_minus_clicked()       { insert_str( "-" ); }
+void calc::on_pushButton_multiply_clicked()    { insert_str( "*" ); }
+void calc::on_pushButton_divide_clicked()      { insert_str( "/" ); }
+
+void calc::on_pushButton_enter_clicked()
+{
+
+}
+
+void calc::on_pushButton_backspace_clicked()
+{
+
 }
--- calc.h
+++ calc.h
@@ -15,6 +15,29 @@ public:
     explicit calc(QWidget *parent = 0);
     ~calc();
 
+private slots:
+    void insert_str( const QString &str );
+    void on_pushButton_n1_clicked();
+    void on_pushButton_n2_clicked();
+    void on_pushButton_n3_clicked();
+    void on_pushButton_n4_clicked();
+    void on_pushButton_n5_clicked();
+    void on_pushButton_n6_clicked();
+    void on_pushButton_n7_clicked();
+    void on_pushButton_n8_clicked();
+    void on_pushButton_n9_clicked();
+    void on_pushButton_n0_clicked();
+    void on_pushButton_open_paren_clicked();
+    void on_pushButton_close_paren_clicked();
+    void on_pushButton_plus_clicked();
+    void on_pushButton_minus_clicked();
+    void on_pushButton_multiply_clicked();
+    void on_pushButton_divide_clicked();
+
+    void on_pushButton_enter_clicked();
+
+    void on_pushButton_backspace_clicked();
+
 private:
     Ui::calc *ui;
 };
